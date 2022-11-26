fb328a62a21972c3db86911f1cd6ba4209705baa
Andrey Krupa <rassor96@gmail.com>
2022-11-26 14:34:04 +0200

    Improve functional, add useImageSize custom hook, add typescript support

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare type Options = {
+  timeout?: number
+};
+
+declare type Dimensions = {
+  width: number;
+  height: number;
+};
+
+declare function getImageSize(url: string, options?: Options): Promise<Dimensions>
+
+declare type UseImageSizeResult = [
+  Dimensions | null,
+  { loading: boolean; error: string | null; },
+];
+
+declare function useImageSize(url: string, options?: Options): UseImageSizeResult;
+
+declare type ExportDefault = {
+  getImageSize,
+  useImageSize,
+};
+
+export default ExportDefault;
--- index.js
+++ index.js
@@ -1,38 +1,67 @@
-const ERROR = {
-  WINDOW_VARIABLE_NOT_FOUND: 'Window variable not found',
-  TIMEOUT: 'Timeout exception',
+const React = require('react');
+
+function getImageSize(url, options = {}) {
+  return new Promise((resolve, reject) => {
+    if (typeof window === 'undefined') {
+      return reject('Window is not defined');
+    }
+
+    if (!url) {
+      return reject('Url is not defined');
+    }
+
+    let timer = null;
+
+    const img = new Image();
+
+    img.addEventListener('load', () => {
+      if (timer) { clearTimeout(timer); }
+
+      resolve({ width: img.naturalWidth, height: img.naturalHeight });
+    });
+
+    img.addEventListener('error', (event) => {
+      if (timer) { clearTimeout(timer); }
+
+      reject(`${event.type}: ${event.message}`);
+    });
+
+    img.src = url;
+
+    if (options.timeout) {
+      timer = setTimeout(() => reject('Timeout'), options.timeout);
+    }
+  });
+}
+
+function useImageSize(url, options = {}) {
+  const [dimensions, setDimensions] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  React.useLayoutEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      setDimensions(null);
+
+      try {
+        const { width, height } = await getImageSize(url, options);
+
+        setDimensions({ width, height });
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetch();
+  }, [url, options]);
+
+  return [dimensions, { loading, error }];
+}
+
+module.exports = {
+  getImageSize,
+  useImageSize,
 };
-
-/**
- * Get dimensions of any image by url
- * @param {string} url
- * @param {number} [rejectTimeout] timeout for reject
- * @return {Promise<{ width: number, height: number }>}
- */
-export default (url, rejectTimeout) => new Promise((resolve, reject) => {
-  if (typeof window === 'undefined') {
-    return reject(ERROR.WINDOW_VARIABLE_NOT_FOUND);
-  }
-
-  let timer = null;
-
-  const img = new Image();
-
-  img.addEventListener('load', () => {
-    if (timer) { clearTimeout(timer); }
-
-    resolve({ width: img.naturalWidth, height: img.naturalHeight });
-  });
-
-  img.addEventListener('error', (event) => {
-    if (timer) { clearTimeout(timer); }
-
-    reject(`${event.type}: ${event.message}`);
-  });
-
-  img.src = url;
-
-  if (rejectTimeout) {
-    timer = setTimeout(() => reject(ERROR.TIMEOUT), rejectTimeout);
-  }
-});
